0325e2774010f0f856956b5681828f9e275674c7
DEV <dev@info.local>
2021-10-08 16:40:03 -0300

    Crud MVC dot net core

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCRazorCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRazorCRUD.Controllers
+{
+    public class AlunoController : Controller
+    {
+        //[Route("Aluno")]
+
+        Aluno alunoModel = new Aluno();
+        public IActionResult Index()
+        {
+            ViewBag.ListaDeAlunos = alunoModel.ListarAluno();
+            return View();
+        }
+
+        [Route("Cadastrar")] //para acessar esse > Aluno/Cadastro
+        public IActionResult Cadastrar(IFormCollection formulario)
+        {
+            Aluno aluno = new Aluno();
+            //recebe os dados do formulário
+            aluno.Nome = formulario["nomeAluno"];
+            aluno.Email = formulario["emailAluno"];
+            aluno.Endereco = formulario["enderecoAluno"];
+            aluno.Telefone = formulario["telefoneAluno"];
+            aluno.Escolaridade = formulario["escolaridadeAluno"];
+
+            aluno.CadastrarAluno(aluno);
+
+            return LocalRedirect("/");
+        }
+
+        [Route("Atualizar")]
+        public IActionResult Atualizar(IFormCollection formulario)
+        {
+            Aluno aluno = new Aluno();
+            //recebe os dados do formulário
+            aluno.Id = int.Parse(formulario["idAluno"]);
+            aluno.Nome = formulario["nomeAluno"];
+            aluno.Email = formulario["emailAluno"];
+            aluno.Endereco = formulario["enderecoAluno"];
+            aluno.Telefone = formulario["telefoneAluno"];
+            aluno.Escolaridade = formulario["escolaridadeAluno"];
+
+            aluno.AtualizarAluno(aluno);
+
+            return LocalRedirect("/");
+        }
+
+        [Route("Cadastro")]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [Route("~/Aluno/{id}")]
+        public IActionResult Remover(int id)
+        {
+            alunoModel.RemoverAluno(id);
+
+            return LocalRedirect("/Aluno");
+        }
+
+        [Route("~/Aluno/Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+            var listaRetornada = alunoModel.BuscarPorId(id);
+            var alunoRetornado = listaRetornada.Find(aluno => aluno.Id == id);
+
+            ViewBag.alunoRetornado = alunoRetornado;
+
+            return View();
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,149 @@
+﻿using MVCRazorCRUD.Context;
+using MVCRazorCRUD.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRazorCRUD.Models
+{
+    public class Aluno : UsuarioBase , IAluno 
+    {
+        public string Escolaridade { get; set; }
+
+        public void AtualizarAluno(Aluno aluno)
+        {
+            var connection = Conexao.GetConnect();
+            connection.Open();
+            var query = "update Alunos set alunoNome = @nome, alunoEmail = @email, alunoEndereco = @endereco, alunoTelefone = @telefone, alunoEscolaridade = @escolaridade where alunoId = @id";
+            var command = new SqlCommand(query, connection);
+
+            command.Parameters.Add("@nome", SqlDbType.VarChar).Value = aluno.Nome;
+            command.Parameters.Add("@email", SqlDbType.VarChar).Value = aluno.Email;
+            command.Parameters.Add("@endereco", SqlDbType.VarChar).Value = aluno.Endereco;
+            command.Parameters.Add("@telefone", SqlDbType.VarChar).Value = aluno.Telefone;
+            command.Parameters.Add("@escolaridade", SqlDbType.VarChar).Value = aluno.Escolaridade;
+            command.Parameters.Add("@id", SqlDbType.Int).Value = aluno.Id;
+            command.ExecuteNonQuery();
+        }
+
+        public List<Aluno> BuscarPorId(int id)
+        {            
+            var connection = Conexao.GetConnect();
+            connection.Open();
+
+            var query = "select * from alunos where alunoId = @id";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
+            var dataSet = new DataSet();
+            var adapter = new SqlDataAdapter(command);
+            adapter.Fill(dataSet);
+
+            var rows = dataSet.Tables[0].Rows;
+
+            List<Aluno> listaAlunos = new List<Aluno>();
+
+            foreach (DataRow item in rows)
+            {
+                var colunas = item.ItemArray;
+
+                Aluno aluno = new Aluno();
+
+                aluno.Id = int.Parse(colunas[0].ToString());
+                aluno.Nome = colunas[1].ToString();
+                aluno.Email = colunas[2].ToString();
+                aluno.Endereco = colunas[3].ToString();
+                aluno.Telefone = colunas[4].ToString();
+                aluno.Escolaridade = colunas[5].ToString();
+                listaAlunos.Add(aluno);
+            }
+            connection.Close();
+            return listaAlunos;
+        }
+
+        public Aluno CadastrarAluno(Aluno aluno)
+        {
+            try
+            {
+                var connection = Conexao.GetConnect();
+                connection.Open();
+                var query = "insert into Alunos (alunoNome, alunoEmail, alunoEndereco, alunoTelefone, alunoEscolaridade) values (@nome, @email, @end, @tel, @esc)";
+
+                var command = new SqlCommand(query, connection);
+
+                command.Parameters.Add("@nome", SqlDbType.VarChar).Value = aluno.Nome;
+                command.Parameters.Add("@email", SqlDbType.VarChar).Value = aluno.Email;
+                command.Parameters.Add("@end", SqlDbType.VarChar).Value = aluno.Endereco;
+                command.Parameters.Add("@tel", SqlDbType.VarChar).Value = aluno.Telefone;
+                command.Parameters.Add("@esc", SqlDbType.VarChar).Value = aluno.Escolaridade;
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+                return aluno;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Aluno> ListarAluno()
+        {
+            try
+            {
+                var connection = Conexao.GetConnect();
+                connection.Open();
+                //query
+                var query = "select * from Alunos";
+                var command = new SqlCommand(query, connection);
+                var dataSet = new DataSet();
+                var adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataSet);
+
+                var rows = dataSet.Tables[0].Rows;
+
+                List<Aluno> listaDeAlunos = new List<Aluno>();
+
+                foreach (DataRow item in rows)
+                {
+                    Aluno aluno = new Aluno();
+                    var colunas = item.ItemArray;
+                    aluno.Id = int.Parse(colunas[0].ToString());
+                    aluno.Nome = colunas[1].ToString();
+                    aluno.Email = colunas[2].ToString();
+                    aluno.Endereco = colunas[3].ToString();
+                    aluno.Telefone = colunas[4].ToString();
+                    aluno.Escolaridade = colunas[5].ToString();
+
+                    listaDeAlunos.Add(aluno);
+                }
+                connection.Close();
+                return listaDeAlunos;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }          
+            
+        }
+
+        public void RemoverAluno(int id)
+        {
+            var connection = Conexao.GetConnect();
+            connection.Open();
+
+            var query = "delete from Alunos where idAluno = @id";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
+            //aqui a query é executada no banco
+            command.ExecuteNonQuery();
+        }
+    }
+}
